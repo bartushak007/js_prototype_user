c45b3b8314b1fbe3c5a2b51175946fb30cef7687
rmnbrtshk <rmnbrtshk@gmail.com>
2019-01-30 12:51:48 +0200

    add local storage mod

--- src/js/main.js
+++ src/js/main.js
@@ -1,28 +1,3 @@
-function signedUser(checkName, checkPassword) {
-  for (var i = 0; i < usersArray.length; i++) {    
-    if (usersArray[i]['name'] == checkName && usersArray[i]['password'] == checkPassword) {
-      signed = usersArray[i];
-      signed.showName();
-      signed.showHeader();
-      signed.showNews();
-      signed.hide();      
-    }
-  }
-}
-
-function counter() {
-  var num = 0;
-  return function() {
-    return num++;
-  };
-}
-
-function logined(x, y) {
-  if (x && y) {
-    signedUser(x, y);   
-  }
-}
-
 function User(name, id, password, repeatPassword) {
   this.id = id();
   this.role = 'user';
@@ -31,28 +6,32 @@ function User(name, id, password, repeatPassword) {
 }
 
 User.prototype = {
+  _headerUserName: document.querySelector('.header__user-name'), 
+  _news: document.querySelector('.news'),
+  _header: document.querySelector('.header'),
+  _registration: document.querySelector('.registration'),
+  _addNews: document.querySelector('.add-news'),
+  _deleteNews: document.querySelector('.delete-news'),
   showNews: function() {
-    document.querySelector('.news').classList.remove('js-none');
+    this._news.classList.remove('js-none');
   },
-  changePassword: function() {
-    document.querySelector('.changePassword').classList.remove('js-none');
-  },
-  showName: function() {
-    document.querySelector('.header__user-name').classList.remove('js-none');
-    document.querySelector('.header__user-name').innerHTML = signed['name'];
+  showName: function() {  
+    this._headerUserName.classList.remove('js-none');
+    this._headerUserName.innerHTML = signed['name'];    
   },
   showHeader: function() {
-    document.querySelector('.header').classList.remove('js-none');
+    this._header.classList.remove('js-none');
   },
   hide: function() {
-    document.querySelector('.registration').classList.add('js-none');
+    this._registration.classList.add('js-none');
   },
   logOut: function() {
     signed = 0;
-    document.querySelector('.news').classList.add('js-none');
-    document.querySelector('.registration').classList.remove('js-none');
-    document.querySelector('.header__user-name').classList.add('js-none');
-    document.querySelector('.header').classList.add('js-none');
+    localStrg.removeItem('user');
+    this._news.classList.add('js-none');
+    this._registration.classList.remove('js-none');
+    this._headerUserName.classList.add('js-none');
+    this._header.classList.add('js-none');
   }
 };
 
@@ -69,42 +48,43 @@ Visitor.prototype = Object.create(User.prototype);
 
 Visitor.prototype.hide = function() {
   User.prototype.hide();
-  document.querySelector('.add-news').classList.add('js-none');
-  document.querySelector('.delete-news').classList.add('js-none');
+  this._addNews.classList.add('js-none');
+  this._deleteNews.classList.add('js-none');  
 };
 
 Visitor.prototype.logOut = function() {
   User.prototype.logOut();
-  document.querySelector('.add-news').classList.remove('js-none');
-  document.querySelector('.delete-news').classList.remove('js-none');
+  this._addNews.classList.remove('js-none');
+  this._deleteNews.classList.remove('js-none');
 };
 
 Admin.prototype = Object.create(User.prototype);
 Admin.prototype.showName = function() {
   User.prototype.showName();
-  document
-    .querySelector('.header__user-name')
-    .classList.add('header__user-name--admin');
+  this._headerUserName.classList.add('header__user-name--admin');
 };
 
 Admin.prototype.showHeader = function() {
   User.prototype.showHeader();
-  document.querySelector('.add-news').classList.remove('js-none');
-  document.querySelector('.delete-news').classList.remove('js-none');
+  this._addNews.classList.remove('js-none');
+  this._deleteNews.classList.remove('js-none');
 };
 
 Admin.prototype.logOut = function() {
-  User.prototype.logOut();
-  document
-    .querySelector('.header__user-name--admin')
-    .classList.remove('header__user-name--admin');
+  User.prototype.logOut();  
+  document.querySelector('.header__user-name--admin').classList.remove('header__user-name--admin');
 };
 
 var setId = counter();
 var usersArray = [];
 var signed;
 var createObjForm = document.forms.createAccount;
+var localStrg = window.localStorage;
+var logOut = document.querySelector('.log-out');
+var signIn = document.forms.signIn;
 
+usersArray.push(new Visitor('Ronny', setId, '123', '123'));
+usersArray.push(new Admin('Adam', setId, '123', '123'));
 
 createObjForm.addEventListener('formIsValid', function() {
   if (
@@ -120,16 +100,50 @@ createObjForm.addEventListener('formIsValid', function() {
   }
 });
 
-var logOut = document.querySelector('.log-out');
 logOut.addEventListener('click', function() {
   signed.logOut();
 });
-
-var signIn = document.forms.signIn;
 
 signIn.addEventListener('formIsValid', function() {
   logined(signIn.elements.name.value, signIn.elements.password.value);
 });
 
-usersArray.push(new Visitor('Ronny', setId, '123', '123'));
-usersArray.push(new Admin('Adam', setId, '123', '123'));
+if (localStrg.user) {
+  for (var i = 0; i < usersArray.length; i++) {
+    if (usersArray[i]['name'] === localStrg.user) {        
+      signed = usersArray[i];
+      signed.showName();
+      signed.showHeader();
+      signed.showNews();
+      signed.hide();     
+    }
+  }    
+}  
+
+function signedUser(checkName, checkPassword) {
+  for (var i = 0; i < usersArray.length; i++) {    
+    if (usersArray[i]['name'] == checkName && usersArray[i]['password'] == checkPassword) {      
+      localStrg.setItem('user', checkName);
+      signed = usersArray[i];
+      signed.showName();
+      signed.showHeader();
+      signed.showNews();
+      signed.hide();      
+    }
+  }
+}
+
+function counter() {
+  var num = 0;
+
+  return function() {
+    return num++;
+  };
+}
+
+function logined(x, y) {
+  if (x && y) {
+    signedUser(x, y);     
+  }
+}
+
